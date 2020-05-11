4fbe1fc9cee63f0003d6e6243aefdbfaf814e55b
gwangwoo <rhkddn5161@naver.com>
2020-05-11 14:10:13 +0900

    first init

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("Im Working. Im JS , Im Work it");
+
+let a = 221;
+let b = a - 5;
+
+a = 4;
+console.log(b, a);
+// to do : finish this
+
+// const what = "String";
+
+// console.log(what);
+
+const what = false;
+const fuck = true;
+
+console.log(fuck, what);
+
+const flo = 3.5;
+console.log(flo);
+
+const arr = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+console.log(arr[2]);
+
+const arl = ["dafad", 123, true];
+
+console.log(arl);
+
+const person = ["gguu", "27", true, "Seoul"];
+
+const gguuInfo = {
+  name: "gguu",
+  age: "27",
+  gender: "Male",
+  isHandsome: true,
+  movie: ["Along the gods", "LOTR", "old boy"],
+  favFood: [{ name: "Kimchi", fatty: false }, { name: "cheese", fatty: true }]
+};
+
+// console.log(gguuInfo.age);
+// console.log(gguuInfo["name"]);
+// console.log(gguuInfo.movie);
+
+console.log(gguuInfo.favFood);
