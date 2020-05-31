a55fe5f44809d6d3f90ec61d89ae9a715c66f99b
gwangwoo <rhkddn5161@naver.com>
2020-06-01 08:44:36 +0900

    200601

--- index.js
+++ index.js
@@ -1,55 +1,18 @@
-console.log("Im Working. Im JS , Im Work it");
+const title = document.querySelector("#title");
+console.log(title);
+const BASE_COLOR = "rgb(52, 73, 74)";
+const OTHER_COLOR = "#7f8c";
 
-let a = 221;
-let b = a - 5;
-
-a = 4;
-console.log(b, a);
-// to do : finish this
-
-// const what = "String";
-
-// console.log(what);
-
-const what = false;
-const fuck = true;
-
-console.log(fuck, what);
-
-const flo = 3.5;
-console.log(flo);
-
-const arr = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-console.log(arr[2]);
-
-const arl = ["dafad", 123, true];
-
-console.log(arl);
-
-const person = ["gguu", "27", true, "Seoul"];
-
-const gguuInfo = {
-  name: "gguu",
-  age: "27",
-  gender: "Male",
-  isHandsome: true,
-  movie: ["Along the gods", "LOTR", "old boy"],
-  favFood: [{ name: "Kimchi", fatty: false }, { name: "cheese", fatty: true }],
-};
-
-// console.log(gguuInfo.age);
-// console.log(gguuInfo["name"]);
-// console.log(gguuInfo.movie);
-
-console.log(gguuInfo.favFood);
-
-console.log(console, gguuInfo.isHandsome);
-
-function sayHello(data, age) {
-  return `Hello ${data} your age ${age} years old`;
+function handleClick() {
+  const cur = title.style.color;
+  if (cur === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
-
-const greetNicolas = sayHello("Nicolas", 14);
-
-console.log(greetNicolas);
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+init();
